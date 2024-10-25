fe7bf8bc5c48a5093a27155770cf3c7645f6253d
shabanashaik-11 <shabanashaikshabana11@gmail.com>
2024-10-25 12:21:31 +0530

    Create Postive & negative.c

---
+++ Postive & negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void  main() 
+ {
+     int n;
+     printf("enter n value ");
+    scanf("%d",&n);
+    if(n>0)
+{
+    printf("postive "); 
+}
+ else  
+ {
+printf("negative ");
+}
+}
