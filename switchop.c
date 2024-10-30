877c42451aa4f6c05b6a93acc14cb593c2429f5b
shabanashaik-11 <shabanashaikshabana11@gmail.com>
2024-10-30 15:01:50 +0530

    Create switchop.c

---
+++ switchop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+int a,b;
+char op;
+printf("enter the values of a and b and op");
+scanf("%d%d %c",&a,&b,&op);
+switch(op)
+{
+case '+':printf("the addition  is %d",a+b);
+break;
+case '-':printf("the subtraction is %d",a-b);
+break;
+case '*':printf("the multiplication is %d",a*b);
+break;
+case '/':printf("the division is %d",a/b);
+break;
+case '%':printf("the module is %d",a%b);
+break;
+default:printf("invalid");
+break;
+}
+
+    return 0;
+}
